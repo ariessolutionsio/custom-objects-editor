b03ca7da8d3b5031a50b6f7c3cd89ef9705bebdf
Jayanth Nimmalapalli <jayanth.nimmalapalli@ariessolutions.io>
2024-03-04 17:58:54 -0600

    fix

--- src/components/entry-point/entry-point.tsx
+++ src/components/entry-point/entry-point.tsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import React, { lazy, useEffect } from 'react';
 import {
   ApplicationShell,
   setupGlobalErrorListener,
@@ -19,11 +19,31 @@ const AsyncApplicationRoutes = lazy(
 // in order to catch possible errors on rendering/mounting.
 setupGlobalErrorListener();
 
-const EntryPoint = () => (
-  <ApplicationShell environment={window.app} applicationMessages={loadMessages}>
-    <AsyncApplicationRoutes />
-  </ApplicationShell>
-);
+const EntryPoint = () => {
+
+  useEffect(() => {
+    const ariesLogoElement = document.createElement('div');
+    const anchor = document.createElement('a');
+    const img = document.createElement('img');
+    img.src = 'https://res.cloudinary.com/dlwdq84ig/image/upload/v1705673742/kkepkrfkpmxqz52cg9ns.png';
+    img.alt='aries-logo';
+    img.style.width = 'inherit';
+    img.style.height = '32px';
+    img.style.marginLeft = '10px';
+    anchor.href = '/aries_dev-1';
+    anchor.appendChild(img);
+    ariesLogoElement.appendChild(anchor);
+    const parent = document.getElementById('loader-for-requests-in-flight')?.parentElement;
+    const loaderElement = document.getElementById('loader-for-requests-in-flight');
+    parent?.insertBefore(ariesLogoElement, loaderElement);
+  },[]);
+  
+  return (
+    <ApplicationShell environment={window.app} applicationMessages={loadMessages}>
+      <AsyncApplicationRoutes />
+    </ApplicationShell>
+  );
+};
 EntryPoint.displayName = 'EntryPoint';
 
 export default EntryPoint;
--- src/context/container-context.tsx
+++ src/context/container-context.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useContext, useEffect } from 'react';
+import React, { PropsWithChildren, useContext } from 'react';
 import map from 'lodash/map';
 import { TCustomObject } from '../types/generated/ctp';
 
@@ -32,23 +32,6 @@ const ContainerProvider: React.FC<Props> = ({ results, children }) => {
     containers: results || [],
     where: `container in (${map(results, ({ key }) => `"${key}"`).join(',')})`,
   };
-
-  useEffect(() => {
-    const ariesLogoElement = document.createElement('div');
-    const anchor = document.createElement('a');
-    const img = document.createElement('img');
-    img.src = 'https://res.cloudinary.com/dlwdq84ig/image/upload/v1705673742/kkepkrfkpmxqz52cg9ns.png';
-    img.alt='aries-logo';
-    img.style.width = 'inherit';
-    img.style.height = '32px';
-    img.style.marginLeft = '10px';
-    anchor.href = '/aries_dev-1';
-    anchor.appendChild(img);
-    ariesLogoElement.appendChild(anchor);
-    const parent = document.getElementById('loader-for-requests-in-flight')?.parentElement;
-    const loaderElement = document.getElementById('loader-for-requests-in-flight');
-    parent?.insertBefore(ariesLogoElement, loaderElement);
-  },[]);
 
   return (
     <containerContext.Provider value={containerContextValue}>
