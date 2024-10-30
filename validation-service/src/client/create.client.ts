03d6398889455a9c114fb9de37ddd4213e8443b6
Behnam Tehrani <behnam777@gmail.com>
2024-10-30 09:10:49 -0400

    use sdk-v2

--- validation-service/src/client/create.client.ts
+++ validation-service/src/client/create.client.ts
@@ -1,54 +1,20 @@
-import fetch from 'node-fetch';
-import {
-  ClientBuilder,
+import { createClient } from './build.client';
 
-  // Import middlewares
-  type AuthMiddlewareOptions, // Required for auth
-  type HttpMiddlewareOptions, // Required for sending HTTP requests
-} from '@commercetools/ts-client';
+import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+
 import { readConfiguration } from '../utils/config.utils';
-import {
-  ByProjectKeyRequestBuilder,
-  createApiBuilderFromCtpClient,
-} from '@commercetools/platform-sdk';
+import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
 
-const projectKey = readConfiguration().projectKey;
-const scopes = [
-  readConfiguration().scope ? (readConfiguration().scope as string) : 'default',
-];
-
-// Configure authMiddlewareOptions
-const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: `https://auth.${readConfiguration().region}.commercetools.com`,
-  projectKey: projectKey,
-  credentials: {
-    clientId: readConfiguration().clientId,
-    clientSecret: readConfiguration().clientSecret,
-  },
-  scopes,
-  httpClient: fetch,
-};
-
-// Configure httpMiddlewareOptions
-const httpMiddlewareOptions: HttpMiddlewareOptions = {
-  host: `https://api.${readConfiguration().region}.commercetools.com`,
-  httpClient: fetch,
-};
-
-// Export the ClientBuilder
-const ctpClient = new ClientBuilder()
-  .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
-  .withClientCredentialsFlow(authMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware() // Include middleware for logging
-  .build();
-
+/**
+ * Create client with apiRoot
+ * apiRoot can now be used to build requests to de Composable Commerce API
+ */
 export const createApiRoot = ((root?: ByProjectKeyRequestBuilder) => () => {
   if (root) {
     return root;
   }
 
-  root = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+  root = createApiBuilderFromCtpClient(createClient()).withProjectKey({
     projectKey: readConfiguration().projectKey,
   });
 
